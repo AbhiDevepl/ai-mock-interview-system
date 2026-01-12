db4b54321414c0832adcfc686b90b11a06969aa7
Developer Studio <developerabhi596@gmail.com>
2026-01-12 20:23:43 +0530

    Firebase is Done

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,21 +1,20 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA21BfcG1WD-zQ1hZDX0s-_mt9WBc00038",
-  authDomain: "prepwise-4287m.firebaseapp.com",
-  projectId: "prepwise-42878",
-  storageBucket: "prepwise-4287a.firebasestorage.app",
-  messagingSenderId: "718591458179",
-  appId: "1:718591458179:web:8290057d79dcd3a761c8f6",
-  measurementId: "G-RHTOBXOHRS",
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.FIREBASE_APP_ID!,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
 };
 
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
-const analytics =
-  typeof window !== "undefined"
-    ? isSupported().then((yes) => (yes ? getAnalytics(app) : null))
-    : null;
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-export { app, analytics };
