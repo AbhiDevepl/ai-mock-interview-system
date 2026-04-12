9f36a17a733d0f5625e6bffe67379c670464be93
Developer Studio <developerabhi596@gmail.com>
2026-04-12 10:59:03 +0530

    feat: initialize React client project with Vite, Tailwind CSS, and basic routing structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Auth from './pages/Auth.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+    </BrowserRouter>
   </StrictMode>,
 )
